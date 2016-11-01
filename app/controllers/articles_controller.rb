286c5a43d27281af1658e18f5f04c55192aec097
kuznetz <s.kuznetzov@mail.ru>
2016-11-01 19:08:53 +0300

    реализация create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
 
-	def new
+	def new # возвращает форму в браузер
 	end
 	def create
-	render plain: params[:article].inspect
+	@article = Article.new(article_params)
+	if @article.valid?
+		@article.save
+	else
+		render action:'new'
+
+	end
+end
+
+	private
+
+	
+
+	def article_params 
+	 
+	params.require(:article).permit(:title,:text)
+	
 end
 
 
