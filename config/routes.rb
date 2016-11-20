96bc7c3204d2ea99834ba037255c01fc7f5b64c9
Skuznetz <s.kuznetzov@mail.ru>
2016-11-20 20:04:36 +0200

    установка devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+get 'persons/profile', as: 'user_root'
+
+  get 'persons/profile'
+
+root 'home#index'
   get 'home/index'
 get 'terms' =>'pages#terms'
 get 'about' =>'pages#about'
